1fee054159bedd0c1896fa0ef23310dff68ee5bb
azizbhk <146841217+azizbhk@users.noreply.github.com>
2024-04-04 01:16:11 +0100

    Update TestDao.java

--- TP5-main/src/main/java/test/TestDao.java
+++ TP5-main/src/main/java/test/TestDao.java
@@ -1,17 +1,17 @@
 package test;
 import java.util.List;
 
-import dao.ProduitDaoImpl;
-import metier.entities.Produit;
+import dao.voyageDaoImpl;
+import metier.entities.voyage;
 
 public class TestDao {
 
 	public static void main(String[] args) {
-		ProduitDaoImpl pdao= new ProduitDaoImpl();
-		Produit prod= pdao.save(new Produit("iphone 8 plus",2800));
+		voyageDaoImpl pdao= new voyageDaoImpl();
+		voyage prod= pdao.save(new voyage("iphone 8 plus",2800));
 		System.out.println(prod);
-		List<Produit> prods =pdao.produitsParMC("Dell");
-		for (Produit p : prods)
+		List<voyage> prods =pdao.voyagesParMC("Dell");
+		for (voyage p : prods)
 		System.out.println(p);
 		}
 }
